e42601c60c08b4e6fa87a5251566447e67c6fac3
Muhammad Awais <muhammad.awais1@arbisoft.com>
2025-11-29 16:47:42 +0500

    show a 3x3 grid

--- src/main.zig
+++ src/main.zig
@@ -6,22 +6,33 @@ var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
 const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
-    const screenHeight = 800;
     const screenWidth = 800;
+    const screenHeight = 800;
     const boardBorderWidth = 4.0;
+    const boardGap = 4.0;
+    const boardTileWidth = screenWidth / 3;
+    const boardTileHeight = screenHeight / 3;
 
     rl.initWindow(screenWidth, screenHeight, "Sudoku");
     defer rl.closeWindow();
 
     rl.setTargetFPS(60);
 
-    const boardShape: rl.Rectangle = .{ .height = screenHeight - boardBorderWidth, .width = screenWidth - boardBorderWidth, .x = boardBorderWidth / 2, .y = boardBorderWidth / 2 };
-
     while (!rl.windowShouldClose()) {
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
 
-        rl.drawRectangleLinesEx(boardShape, 4.0, .black);
+        rl.drawRectangleLinesEx(.{ .x = boardGap, .y = boardGap, .width = screenWidth - (boardGap * 2), .height = screenHeight - (boardGap * 2) }, boardBorderWidth, .black);
+
+        // vertical lines
+        for (1..3) |i| {
+            rl.drawRectangle(@intCast(i * boardTileWidth), boardGap, boardBorderWidth, (screenHeight - (boardGap * 2)), .black);
+        }
+
+        // horizontal lines
+        for (1..3) |i| {
+            rl.drawRectangle(boardGap, @intCast(i * boardTileHeight), (screenWidth - (boardGap * 2)), boardBorderWidth, .black);
+        }
     }
 }
